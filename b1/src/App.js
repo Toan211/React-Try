c3a6632af90533733de5c64aa4e6d7566f41eb3a
boynuibon3s <boynuibon3s@gmail.com>
2023-04-07 01:58:23 +0700

    first jsx

--- b1/src/App.js
+++ b1/src/App.js
@@ -1,14 +1,12 @@
-
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Employee/>
         <a
           className="App-link"
           href="https://reactjs.org"
