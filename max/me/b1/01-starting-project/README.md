9d24cb2a23d528b4c294c5823e670a8289081ec3
boynuibon3s <boynuibon3s@gmail.com>
2023-11-10 20:51:09 +0700

    component vs props

--- max/me/b1/01-starting-project/README.md
+++ max/me/b1/01-starting-project/README.md
@@ -4,6 +4,7 @@ This using available template, not sure what is the diff tho
 
 1. let's create a function Header as component!
 
-2.
+2. Dynamic content and dynamic path
+
 
 
--- max/me/b1/01-starting-project/src/App.jsx
+++ max/me/b1/01-starting-project/src/App.jsx
@@ -1,25 +1,63 @@
+import reactImg from "./assets/react-core-concepts.png";
+import reactImgComponents from "./assets/components.png";
+
+const reactDes = ["Fundamental ", "Core ", "To try "];
+
+let genRandomInt = (max) => {
+	return Math.floor(Math.random() * (max + 1));
+};
+
 function Header() {
-  return (
-    <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
+	let genDes = reactDes[genRandomInt(reactDes.length)];
+	return (
+		<header>
+			<img src={reactImg} alt="Stylized atom" />
+			<h1>React Essentials</h1>
+			<p>
+				{genDes} React concepts you will need for almost any app you are going
+				to build!
+			</p>
+		</header>
+	);
+}
+
+function CoreConcept(props) {
+	return (
+		<li>
+			<img src={props.image} alt={props.title} />
+			<h3>{props.title}</h3>
+			<p>{props.des}</p>
+		</li>
+	);
 }
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<main>
+				<section id="core-concepts">
+					<h2>Core concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Component"
+              des="The core UI build block"
+              image={reactImgComponents}
+              />
+              <CoreConcept
+             
+              />
+              <CoreConcept
+              
+              />
+              <CoreConcept
+              
+              />
+          </ul>
+				</section>
+			</main>
+		</div>
+	);
 }
 
 export default App;
