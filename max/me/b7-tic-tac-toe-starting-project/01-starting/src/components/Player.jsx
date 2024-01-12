2290a7fc1c9dbd345514685efaeff4264d046c55
boynuibon3s <boynuibon3s@gmail.com>
2024-01-12 09:39:11 +0700

    input field when click

--- max/me/b7-tic-tac-toe-starting-project/01-starting/src/components/Player.jsx
+++ max/me/b7-tic-tac-toe-starting-project/01-starting/src/components/Player.jsx
@@ -1,11 +1,30 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+	const [isEditting, setIsEditting] = useState(false);
+
+	function handleEditClick(edittingState) {
+		if (!edittingState) {
+			setIsEditting(true);
+		} else if (edittingState) {
+			setIsEditting(false);
+		}
+		console.log(edittingState);
+	}
+
 	return (
 		<li>
 			<span className="player">
-				<span className="player-name">{name}</span>
+				{!isEditting ? (
+					<span className="player-name">{name}</span>
+				) : (
+					<input type="text" required/>
+				)}
 				<span className="player-symbol">{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={() => handleEditClick(isEditting)}>
+				{!isEditting ? "Edit" : "Save"}
+			</button>
 		</li>
 	);
 }
